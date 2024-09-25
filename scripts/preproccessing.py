e86558eaa9879cc56c18dec96c550db0aed79c86
charlieamad <camad@student.unimelb.edu.au>
2024-09-25 18:42:13 +1000

    creating add_data, function which adds all of our external data to our already existing house dataframes

--- scripts/preproccessing.py
+++ scripts/preproccessing.py
@@ -414,3 +414,136 @@ def extend_inflation(df, data):
     plt.show()
 
     return extended_df
+
+def extend_percentage(df, data):
+    """
+    Function to extend range of data to our required years from 2006-2029
+    Accepts a dataframe 'df' with the data that needs to be extended, and a string 'data'
+    representing the name of the data we are extending
+    Displays a scatterplot of our newly extended data and returns a dataframe, 'extended_df',
+    which contains all of our extended data for our desired years
+    """
+    
+    df.replace('-', np.nan, inplace=True) # replace '-' values with NaN
+
+    # extract the columns that have majority not NaN values
+    columns = get_majority_non_na_columns(df)
+    df = df[columns]
+
+    # impute any remaining NaN values as 0
+    df.replace(np.nan, 0, inplace=True)
+
+    # create years to be extrapolated from
+    years = np.array(list(df))
+    print(years)
+    extended_data = []
+    extended_years = np.arange(2006, 2030, 1)
+
+    # extend the data for each desired year for each SA2 region
+    for index, row in df.iterrows():
+        values = row.values  # get the values for the region
+
+        # create a linear interpolation function
+        linear_interp = interp1d(years, values, kind='linear', fill_value="extrapolate")
+
+        # generate values from 2006 to 2029
+        extended_values = linear_interp(extended_years)
+
+        # ensure no extrapolated values are below 0
+        extended_values[extended_values < 0] = 0
+        
+        # store the results
+        extended_data.append(extended_values)
+
+    # convert to dataframe
+    extended_df = pd.DataFrame(extended_data, columns=extended_years, index=df.index)
+
+    # now lets plot out data:
+    sampled_regions = df.sample(n=10, random_state=42)
+
+    # create colours
+    colors = plt.cm.viridis(np.linspace(0, 1, len(sampled_regions)))
+
+    plt.figure(figsize=(10, 6))
+    for color, (index, row) in zip(colors, sampled_regions.iterrows()):
+        # plot extended data
+        plt.plot(extended_years, extended_df.loc[index], color=color, label=index + ' Extended')
+        # plot original data points
+        plt.plot(years, row, 'o', color=color)  
+
+    plt.title(f'Extrapolation of {data} to the years 2006-2029')
+    plt.xlabel('Year')
+    plt.ylabel(f'{data}')
+    plt.xticks(np.arange(2005, 2030, 1), rotation=90)
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Regions")
+
+    plt.grid(True)
+    plt.show()
+
+    return extended_df
+
+def add_data(df):
+    """
+    Function created to add external datasets to our houses dataframes, inputting the correct
+    values depending on each house's year and SA2 region.
+    Accepts 'df' as input, a dataframe of house info. and returns the same dataframe with the 
+    external data appended.
+    Relies on 'df' having a 'Year' column with type string.
+    """
+    extended_homelessness_df = pd.read_csv('../data/curated/extrapolated_homelessness_data.csv').set_index('SA2_name_2021')
+    extended_ave_household_size_df = pd.read_csv('../data/curated/extrapolated_ave_household_size.csv').set_index('SA2_name_2021')
+    extended_business_df = pd.read_csv('../data/curated/extrapolated_business.csv').set_index('SA2_name_2021')
+    extended_income_df = pd.read_csv('../data/curated/extrapolated_income.csv').set_index('SA2_name_2021')
+    extended_housing_index_df = pd.read_csv('../data/curated/extrapolated_housing_index.csv')
+    extended_cpi_without_housing_df = pd.read_csv('../data/curated/extrapolated_CPI_without_housing.csv')
+    extended_median_age_df = pd.read_csv('../data/curated/extrapolated_median_age.csv').set_index('SA2_name_2021')
+    extended_median_rent_df = pd.read_csv('../data/curated/extrapolated_median_rent.csv').set_index('SA2_name_2021')
+    extended_percentage_aboriginal_torres_straight_df = pd.read_csv('../data/curated/extrapolated_percentage_aboriginal_torres_straight.csv').set_index('SA2_name_2021')
+    extended_percentage_australian_citizen_df = pd.read_csv('../data/curated/extrapolated_percentage_australian_citizen.csv').set_index('SA2_name_2021')
+    extended_percentage_overseas_born_df = pd.read_csv('../data/curated/extrapolated_percentage_overseas_born.csv').set_index('SA2_name_2021')
+    extended_percentage_rentals_df = pd.read_csv('../data/curated/extrapolated_percentage_rentals.csv').set_index('SA2_name_2021')
+
+
+    extended_dfs = [
+        (extended_homelessness_df, 'Number of Homeless Persons'),
+        (extended_ave_household_size_df, 'Average Household Size'),
+        (extended_business_df, 'Number of Businessess'),
+        (extended_income_df, 'Median Icome'),
+        (extended_median_age_df, 'Median Age'),
+        (extended_median_rent_df, 'Median Weekly Rent'),
+        (extended_percentage_aboriginal_torres_straight_df, 'Percentage Aboriginal or Torres Strait Islander'),
+        (extended_percentage_australian_citizen_df, 'Percentage Australian Citizen'),
+        (extended_percentage_overseas_born_df, 'Percentage Overseas Born'),
+        (extended_percentage_rentals_df, 'Percentage of Rental Properties')
+    ]
+
+    # Loop through each extended dataframe and merge it with df
+    for extended_df, col_name in extended_dfs:
+        # Function to get value or impute mean value for the year if KeyError occurs
+        def get_value_or_mean(sa2_name, year):
+            try:
+                # Attempt to retrieve the value from the extended DataFrame
+                value = extended_df.loc[sa2_name, year]
+                return value
+            except KeyError:
+                # If SA2 region or year is not found, return the mean value for that year
+                mean_value = extended_df[year].mean()
+                return mean_value
+
+        # Add the new column to the original DataFrame
+        df[col_name] = df.apply(lambda row: get_value_or_mean(row['SA2_NAME21'], row['Year']), axis=1)
+
+    # now lets do the same for inflation
+    extended_housing_index_df = extended_housing_index_df.stack().reset_index()
+    extended_housing_index_df.columns = ['Metric', "Year", 'Housing Index']
+    extended_housing_index_df = extended_housing_index_df.drop('Metric', axis=1)
+
+    df = df.merge(extended_housing_index_df, on='Year', how='left')
+
+    extended_cpi_without_housing_df = extended_cpi_without_housing_df.stack().reset_index()
+    extended_cpi_without_housing_df.columns = ['Metric', "Year", 'CPI Without Housing']
+    extended_cpi_without_housing_df = extended_cpi_without_housing_df.drop('Metric', axis=1)
+
+    df = df.merge(extended_cpi_without_housing_df, on='Year', how='left')
+
+    return df
